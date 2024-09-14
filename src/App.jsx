fa9b6096c69137c7a88ef6974e97394c459bf959
xoyn222 <kostuk186@gmail.com>
2024-09-14 23:33:13 +0300

    reAdd

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Header from "./comp/Header/Header.jsx";
+import Card from "./comp/Card/Card.jsx"; // Импортируем карточки товаров
+import {assets} from "./assets/assets";
+
+const App = () => {
+    // Массив с товарами
+    const products = [
+        {
+            name: 'Спортивки "Джинсы"',
+            price: '120',
+            image: assets.jeans,
+        },
+        {
+            name: 'Футболка "Рокки Бальбоа"',
+            price: '20',
+            image: assets.t_shirt,
+        },
+        {
+            name: 'Лонгслив "Подшарил"',
+            price: '40',
+            image: assets.long,
+        },
+        {
+            name: 'Xуди "Ахуительный тренд"',
+            price: '60',
+            image: assets.hood,
+        },
+    ];
+
+    return (
+        <>
+            <Header />
+            <div className="product-list">
+                {products.map((product, index) => (
+                    <Card key={index} product={product} />
+                ))}
+            </div>
+        </>
+    );
+};
+
+export default App;
---
+++ src/assets/assets.js
@@ -0,0 +1,23 @@
+import shopping_cart from './shopping_cart.png'
+import bl from './bl.png'
+import menu_icon from './menu_icon.png'
+import search_icon from './search_icon.png'
+import jeans from './jeans.png'
+import hood from './hood.png'
+import long from './long.png'
+import t_shirt from './t_shirt.png'
+
+export const assets = {
+    menu_icon,
+    shopping_cart,
+    bl,
+    long,
+    hood,
+    search_icon,
+    jeans,
+    t_shirt
+
+}
+
+
+
